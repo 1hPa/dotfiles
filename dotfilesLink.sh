d49e1c444e96f4492300c458e4801f6e3f755804
suga <suga.programmer@gmail.com>
2020-02-06 23:07:38 +0900

    first commit

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,8 @@
-DOT_FILES(.bashrc .bash_profile .zshrc )
-for file in ${DOT_FILES[@]}
+DOTPATH=~/.dotfiles
+
+for f in .??*
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    [ "$f" = ".git" ] && continue
+
+    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
